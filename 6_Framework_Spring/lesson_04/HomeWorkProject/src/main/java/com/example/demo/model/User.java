1947413804523e60195c32133e9ed094e13851f4
Konstantin <yarohno@gmail.com>
2024-10-29 19:55:48 +0300

    Сommitted on вт 29.10.2024 07:55:48 PM MSK

--- 6_Framework_Spring/lesson_04/HomeWorkProject/src/main/java/com/example/demo/model/User.java
+++ 6_Framework_Spring/lesson_04/HomeWorkProject/src/main/java/com/example/demo/model/User.java
@@ -1,18 +1,35 @@
 package com.example.demo.model;
 
-import lombok.Getter;
-import lombok.Setter;
-
 import java.util.Objects;
 
-@Setter
-@Getter
+
 public class User {
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
     private Long id;
-
     private String firstName;
-
     private String lastName;
 
     @Override
